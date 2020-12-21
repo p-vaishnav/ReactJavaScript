cb948e5d823f98e3ccff3af7b953db5f4bafc83c
Vaishnav Pureddiwar <vpureddiwar@ptc.com>
2020-12-21 16:40:22 +0530

    a

---
+++ lcomernbootcamp/testback/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 9000;
+
+app.get('/Login',(req,res)=>{
+    res.send('You are logged in');
+});
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+});
+
+
+
+app.listen(port,()=>{
+    console.log("Server is up and runing");
+});
+
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
