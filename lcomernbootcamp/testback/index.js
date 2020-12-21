83cc3aac02bec0006c748bbe59b5f7c1db310b6a
Vaishnav Pureddiwar <vpureddiwar@ptc.com>
2020-12-21 18:14:44 +0530

    h

--- lcomernbootcamp/testback/index.js
+++ lcomernbootcamp/testback/index.js
@@ -5,6 +5,11 @@ const port = 9000;
 app.get('/Login',(req,res)=>{
     res.send('You are logged in');
 });
+
+app.get('/SignUp',(req,res)=>{
+    res.send('Plese choose Instagram,Facebook,WhatsApp');
+});
+
 app.get('/',(req,res)=>{
     res.send('Hello World!');
 });
